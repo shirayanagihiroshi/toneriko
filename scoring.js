c9d35fb0cde0a0dee7d5c48f6bbde19f6138616a
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2021-12-08 21:12:51 +0900

    あと順不同

--- scoring.js
+++ scoring.js
@@ -3,12 +3,29 @@
 //------モジュールスコープ変数s--------
   const fs       = require('fs'),
         iconv    = require('iconv-lite');
-  let   records, record, data, str, i, j;
+  let   answer, records, record, data, str, i, j;
 
 //------ユーティリティメソッドs--------
-  // 一問ごとのマークシートの個数と正答、得点を設定
-  //    ア,イ,ウ,エ,オ,カ,キ,ク,ケ,コ,サ,シ,ス,セ,ソ,タ,チ,ツ,テ,ト,ナ,ニ,ヌ,ネ,ノ,ハ,ヒ,フ,ヘ,ホ
-  //and =
+  // ア,イ,ウ,エ,オ,カ,キ,ク,ケ,コ,サ,シ,ス,セ,ソ,タ,チ,ツ,テ,ト,ナ,ニ,ヌ,ネ,ノ,ハ,ヒ,フ,ヘ,ホ
+  // 答えの設定
+  // 得点を与える問題ごとに要素を作る
+  // ans   : 正解(配列)
+  // score : 配点
+  // kind  : 0 指定の解のみ許容
+  //         1 順不同でよい(この場合は上記scoreをそれぞれに与える)
+  answer = [{ans   : [5],
+             score : 2,
+             kind  : 0},
+             {ans  : [1,1,0,0],
+             score : 2,
+             kind  : 0},
+             {ans  : [3],
+             score : 3,
+             kind  : 0},
+             {ans  : [1,9,0],
+             score : 3,
+             kind  : 0}
+           ];
 //------ユーティリティメソッドe--------
 
 //------メイン部s--------
@@ -19,10 +36,31 @@
 
        records = str.split(/\n/);        // 改行で区切る。これが一人分。
        for (i = 0; i < records.length; i++) {
+         let markPos = 2, //マー君の仕様上、最初は通番、次はIDで、2から回答の内容
+             personScore = 0;
          record = records[i].split(','); // さらにコンマで区切る。これがマーク単位。
-         for (j = 0; j < record.length; j++) {
-           console.log(record[j]);
+                                         // 一人分を採点できる形にしたもの
+         // 答えを設定したかたまり毎のloop
+         for (j = 0; j < answer.length; j++) {
+           let k, incorrectFlg = false;
+
+           // 指定の解(の順)のみ許容
+           if ( answer[j].kind == 0 ) {
+             for (k = 0; k < answer[j].ans.length; k++, markPos++) {
+               if ( answer[j].ans[k] != record[markPos]) {
+                 incorrectFlg = true;
+               }
+             }
+             if (!incorrectFlg) {
+               personScore += answer[j].score;
+             }
+
+           // 順不同
+           } else if ( answer[j].kind == 0)  {
+
+           }
          }
+         console.log(record[0] + ':' + personScore);
        }
      }
   } else {
