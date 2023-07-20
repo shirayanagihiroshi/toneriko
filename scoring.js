b7c5e86b3a5fc5bfcfbe4c34323c15bf881a019a
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2023-07-20 17:53:34 +0900

    2023/7/20微積単元テストの正解設定と、合計点のみでなく各問の得点を表示するよう変更

--- scoring.js
+++ scoring.js
@@ -13,18 +13,30 @@
   // score : 配点
   // kind  : 0 指定の解のみ許容
   //         1 順不同でよい(この場合は上記scoreをそれぞれに与える)
-  answer = [{ans  : [4,8,4,'A',8],    score : 8, kind : 0},
-             {ans : [1,4,'B',1,'A'],  score : 8, kind : 0},
-             {ans : [7,'A',5,6,'A'],  score : 8, kind : 0},
-             {ans : [1,'A',7,4,5],    score : 8, kind : 0},
-             {ans : [5,9,8,9,8],      score : 8, kind : 0},
-             {ans : [5,1,6],          score : 8, kind : 0},
-             {ans : [4],              score : 5, kind : 0},
-             {ans : [1,8,5,1,8,5],    score : 8, kind : 0},
-             {ans : [6,6,6],          score : 8, kind : 0},
-             {ans : ['B',6,1,5],      score : 8, kind : 0},
-             {ans : [7,4,3,6],        score : 5, kind : 0},
-             {ans : [8,6,4,'C'],      score : 8, kind : 0}
+  answer = [{ans  : [4,'C',6],        score : 6, kind : 0},
+             {ans : [8,6],            score : 6, kind : 0},
+             {ans : [1,5],            score : 2, kind : 0},
+             {ans : [4],              score : 2, kind : 0},
+             {ans : [6],              score : 2, kind : 0},
+             {ans : [4,'A',5],        score : 6, kind : 0},
+             {ans : [7,6],            score : 5, kind : 0},
+             {ans : [1,7],            score : 5, kind : 0},
+             {ans : [9,5,8,4,5],      score : 6, kind : 0},
+
+             {ans : [5],              score : 2, kind : 0},
+             {ans : [1,6,5],          score : 6, kind : 0},
+             {ans : [1],              score : 2, kind : 0},
+             {ans : [1],              score : 2, kind : 0},
+             {ans : [4],              score : 2, kind : 0},
+             {ans : ['A'],            score : 2, kind : 0},
+             {ans : [8,6,'C'],        score : 6, kind : 0},
+             {ans : [9],              score : 4, kind : 0},
+             {ans : [4,'B',3],        score : 4, kind : 0},
+             {ans : [4,'B',3],        score : 6, kind : 0},
+             {ans : [6,3,3,4,5,8],    score : 6, kind : 0},
+             {ans : [5],              score : 6, kind : 0},
+             {ans : [1],              score : 6, kind : 0},
+             {ans : [5],              score : 6, kind : 0}
 	    
            ];
 
@@ -44,6 +56,7 @@
        for (i = 0; i < records.length; i++) {
          let markPos = 2, //マー君の仕様上、最初は通番、次はIDで、2から回答の内容
              personScore = 0,
+             personScoreStr = "", // 設問(加点する部分)ごとの得点を表示。これらの合計がpersonScoreになるはず。
              record = records[i].split(','); // さらにコンマで区切る。これがマーク単位。
                                              // 一人分を採点できる形にしたもの
          // 答えを設定したかたまり毎のloop
@@ -62,6 +75,9 @@
              }
              if (!incorrectFlg) {
                personScore += answer[j].score;
+               personScoreStr += ',' + String(answer[j].score);
+             } else {
+               personScoreStr += ',0';
              }
 
            // 順不同
@@ -76,13 +92,16 @@
              for (k = 0; k < answer[j].ans.length; k++) {
                if ( lst.includes( String(answer[j].ans[k]) ) ) {
                  personScore += answer[j].score;
+                 personScoreStr += ',' + String(answer[j].score);
+               } else {
+                 personScoreStr += ',0';
                }
              }
            }
          }
          // 結果を書き込み
          if (i != 0) {
-           fs.appendFileSync(outputFile, record[0] + ',' + record[1] + ',' + String(personScore) + '\n');
+           fs.appendFileSync(outputFile, record[0] + ',' + record[1] + personScoreStr +  ',' + String(personScore) + '\n');
          }
        }
      }
