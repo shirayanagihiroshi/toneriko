2423920204ba5c947b4b2808439f765a6cbae24b
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-06-12 18:53:46 +0900

    第6回追加開始

---
+++ docs/programmingCourse06.js
@@ -0,0 +1,21 @@
+﻿//sample
+function changeColor() {
+  let element = document.getElementById("ID1238");
+  element.style.color = "blue";
+}
+
+function kakunin01() {
+  let textField = document.querySelector('#inputBox01');
+
+  Function (textField.value)();
+
+  return;
+}
+
+function kakunin02() {
+  let textField = document.querySelector('#inputBox02');
+
+  Function (textField.value)();
+
+  return;
+}
