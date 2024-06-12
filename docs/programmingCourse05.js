ba786c7d854f1222214bfa29c255e2e0592e837d
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-06-12 12:27:03 +0900

    第5回へ追記

--- docs/programmingCourse05.js
+++ docs/programmingCourse05.js
@@ -1,6 +1,28 @@
-//sample
+﻿//sample
 function removeTomato () {
   let element = document.getElementById("ID1234");
   element.remove();
 }
 
+function addPotate () {
+  let parent = document.getElementById("ID1235");
+  let child = document.createElement('li');
+  child.innerHTML = "ポテト";
+  parent.appendChild(child);
+}
+
+function kakunin01() {
+  let textField = document.querySelector('#inputBox01');
+
+  Function (textField.value)();
+
+  return;
+}
+
+function kakunin02() {
+  let textField = document.querySelector('#inputBox02');
+
+  Function (textField.value)();
+
+  return;
+}
