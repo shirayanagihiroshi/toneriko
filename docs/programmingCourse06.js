ad0665dac30fe0878aac40e5ebf2affdd326b56c
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-06-14 13:50:23 +0900

    第6回へ問題追加

--- docs/programmingCourse06.js
+++ docs/programmingCourse06.js
@@ -12,8 +12,40 @@ function kakunin01() {
   return;
 }
 
+  window.onload = function () {
+    let myTimer;
+    let myPosition = 0;
+    let myDirection = "right";
+ 
+    function myMove(){
+      let element = document.getElementById("ID1244");
+      if (myDirection == "right") {
+        myPosition = myPosition + 5;
+        if (myPosition == 100) {
+          myDirection = "left";
+        }
+      } else {
+        myPosition = myPosition - 5;
+        if (myPosition == 0) {
+          myDirection = "right";
+        }
+      }
+      element.style.left = String(myPosition) + "px"; 
+    }
+
+    myTimer = setInterval(myMove, 100);
+  }
+
 function kakunin02() {
   let textField = document.querySelector('#inputBox02');
+
+  Function (textField.value)();
+
+  return;
+}
+
+function kakunin03() {
+  let textField = document.querySelector('#inputBox03');
 
   Function (textField.value)();
 
