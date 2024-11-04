37ce414e8e7b75f0659de885b03f6448aa774109
shirayanagi hiroshi <shirayanagi.hiroshi@gmail.com>
2024-11-04 22:43:30 +0900

    第10回に追記

--- docs/programmingCourse10.js
+++ docs/programmingCourse10.js
@@ -1,5 +1,15 @@
 ﻿//10
 
+  // 待ち行列
+  let waitList = [];
+  let canvas;
+  let context;
+  let kyakuTimerId = 0;
+  let miseTimerId = 0;
+  let countupId = 0;
+  let total_time = 0;
+  let total_num = 0;
+
 // 描画ライブラリ
   function drawPersonA(x, y, person, context) {
     context.fillStyle = person.personColor;
@@ -18,15 +28,41 @@
     context.fillRect(x + 16, y + 1,   3, 9 ); //腕
   }
 
-  // 待ち行列
-  let waitList = [];
+  function viewPersons() {
+    let i;
+
+    context.clearRect(0, 0, 500, 100);
+
+    for (i = 0; i < waitList.length; i++) {
+      drawPersonA(20*i, 0, waitList[i], context);
+    }
+  }
+
+  function countup() {
+    let i;
+
+    for (i = 0; i < waitList.length; i++) {
+      waitList[i].waitTime += 100;
+    }
+  }
 
   function enqueue(p) {
     waitList.push(p);
   }
 
-  function dequeue(p) {
-    waitList.shift();
+  function dequeue() {
+    if(waitList.length > 0) {
+      p = waitList.shift();
+      total_time = total_time + p.waitTime;
+      total_num++;
+
+      let obj = document.getElementById('ID1259');
+      let heikin = (total_time/1000)/total_num;
+      heikin = heikin * 10; //ここからの3行で小数第一位で四捨五入
+      heikin = Math.round(heikin);
+      heikin = heikin / 10;
+      obj.innerHTML = "お客さんの平均待ち時間は" + heikin + "分";
+    }
   }
 
   // 一人を作る
@@ -49,14 +85,39 @@
     return Math.floor(Math.random() * (m - (n - 1)) + n);
   }
 
+  function miseTimer(){
+    let mise = document.getElementById('ID1257');
+    //100msはほぼ0。0から2倍までの平均がプルダウン指定した値
+    miseTimerId = setTimeout(miseTimer, Number(mise.value)*1000);
+    dequeue();
+    viewPersons();
+  }
+
+  function kyakuTimer(){
+    let kyaku = document.getElementById('ID1258');
+    //100msはほぼ0。0から2倍までの平均がプルダウン指定した値
+    let interval = getRandam(100,Number(kyaku.value)*1000*2);
+    kyakuTimerId = setTimeout(kyakuTimer, interval);
+
+    enqueue(makePerson());
+    viewPersons();
+  }
+
+  function buttonClick(){
+    clearTimeout(kyakuTimerId);
+    clearTimeout(miseTimerId);
+    
+    let kyaku = document.getElementById('ID1258');
+    let interval = getRandam(100,Number(kyaku.value)*1000*2);
+    kyakuTimerId = setTimeout(kyakuTimer, interval);
+
+    let mise = document.getElementById('ID1257');
+    miseTimerId = setTimeout(miseTimer, Number(mise.value)*1000);
+
+    countupId = setInterval(countup, 100);
+  }
+
 window.onload = function () {
-
-  let canvas, context;
-
   canvas = document.getElementById('ID1256');
   context = canvas.getContext('2d');
-
-  let p = makePerson();
-  drawPersonA(0, 0, p, context)
 }
-
