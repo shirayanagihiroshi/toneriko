e7e6c2fbbd4bb8cf0eebd660ad484ddbca031af5
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-11-15 09:33:06 +0900

    第12回追加開始

---
+++ docs/programmingCourse12.js
@@ -0,0 +1,28 @@
+﻿//12
+
+window.onload = function () {
+}
+
+// 例題1　文字列の比較でチェックする
+function kakunin01() {
+  confirmationTemplate('#inputBox01', '#result01', function (testStr) {
+
+    if (testStr == "お") {
+      return true; 
+    } else {
+      return false;
+    }
+  });
+}
+
+// 例題2　文字列の比較でチェックする
+function kakunin02() {
+  confirmationTemplate('#inputBox02', '#result02', function (testStr) {
+
+    if (testStr == "あお") {
+      return true; 
+    } else {
+      return false;
+    }
+  });
+}
