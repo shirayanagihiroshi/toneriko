b0541b60c876e96e04ac290cf90cd89773320421
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-11-27 13:03:46 +0900

    第13回完成

--- docs/programmingCourse13.js
+++ docs/programmingCourse13.js
@@ -1,5 +1,10 @@
 ﻿//13
 
+// 小数第3位で四捨五入
+function roundAt3decimalpoint(x) {
+  return (Math.round(x * 100) / 100);
+}
+
 window.onload = function () {
 }
 
@@ -41,11 +46,57 @@ function kakunin02() {
   });
 }
 
+// 例題3　実行してみてチェックする
 function kakunin03() {
+  confirmationTemplate('#inputBox03', '#result03', function (testStr) {
+    let codeStr =
+      testStr               +
+      ';'                   +
+      'let r;'              +
+      'if(myMedian([2, 5, 1]) == 2       && ' +
+      '   myMedian([1, 9, 8, 3]) == 5.5  && ' +
+      '   myMedian([2,1,4,7,2,4,5,8,9,3,6,2,7,5,4,6,9,1,2,5,6,7,7,3,5]) == 5)  {'  +
+      '  r = true;'         +
+      '} else {'            +
+      '  r = false;'        +
+      '}'                   +
+      'return r';
+    return Function (codeStr)();
+  });
 }
 
+// 例題4　実行してみてチェックする
 function kakunin04() {
+  confirmationTemplate('#inputBox04', '#result04', function (testStr) {
+    let codeStr =
+      testStr               +
+      ';'                   +
+      'let r;'              +
+      'if(roundAt3decimalpoint(myVariance([1,3,5,6,7,8,9])) == 6.82 && ' +
+      '   roundAt3decimalpoint(myVariance([1,5,3,6,8,4,3,6,2,8,4])) == 4.79 )  {'  +
+      '  r = true;'         +
+      '} else {'            +
+      '  r = false;'        +
+      '}'                   +
+      'return r';
+    return Function (codeStr)();
+  });
 }
 
+// 例題5　実行してみてチェックする
 function kakunin05() {
+  confirmationTemplate('#inputBox05', '#result05', function (testStr) {
+    let codeStr =
+      testStr               +
+      ';'                   +
+      'let r;'              +
+      'if(roundAt3decimalpoint(myCC([[5,3],[6,5],[4,7],[10,12],[8,7],[6,9],[9,10],[2,3],[2,5],[7,7]])) == 0.79 && ' +
+      '   roundAt3decimalpoint(myCC([[1,10],[2,10],[3,6],[4,6],[5,4],[6,4],[6,3],[8,2],[8,1],[10,3]])) == -0.89 )  {'  +
+      '  r = true;'         +
+      '} else {'            +
+      '  r = false;'        +
+      '}'                   +
+      'return r';
+    return Function (codeStr)();
+  });
 }
